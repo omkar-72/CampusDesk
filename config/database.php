d6fd35844dcb682013a233627b5a163bb924262b
Ganesh Mane <ganesh.vitthal.mane@gmail.com>
2026-09-12 15:12:11 +0530

    Git ignore files created

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ $host = "localhost";
 $port = "5432";
 $dbname = "campusdesk";
 $user = "postgres";
-$password = "Shri@2005";
+$password = "";
 
 $conn = pg_connect(
     "host=$host port=$port dbname=$dbname user=$user password=$password"
