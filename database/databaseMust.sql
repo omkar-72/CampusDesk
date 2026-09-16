9f3e650a51d9e22bc94b8fb157f5c6680921c28a
Omkar Gavane <gavaneomkar24@gmail.com>
2026-09-16 19:50:10 +0530

    Add database setup file

---
+++ database/databaseMust.sql
@@ -0,0 +1,26 @@
+INSERT INTO roles (role_name)
+VALUES
+('STUDENT'),
+('AUTHORITY'),
+('ADMIN');
+
+
+INSERT INTO departments (department_name, status)
+VALUES
+('Computer Science', TRUE),
+('Information Technology', TRUE),
+('Microbiology', TRUE),
+('Biotechnology', TRUE),
+('Commerce', TRUE),
+('Arts', TRUE),
+('Mechanical Engineering', TRUE),
+('Civil Engineering', TRUE),
+('Electrical Engineering', TRUE),
+('Electronics and Telecommunication', TRUE),
+('BBA', TRUE),
+('BCA', TRUE),
+('MBA', TRUE),
+('MCA', TRUE),
+('M.Sc Computer Science', TRUE),
+('M.Sc Microbiology', TRUE),
+('M.Sc Biotechnology', TRUE);
